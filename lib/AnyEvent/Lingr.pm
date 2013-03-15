d6a34dec5f720c11407005758d564ffcca53f4d7
Daisuke Murase <typester@cpan.org>
2013-03-15 11:46:57 +0900

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a6e6cd7..9d8edc9 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,4 +1,8 @@
     Revision history for Perl extension AnyEvent::Lingr
    
    +0.02 2013-03-15T11:45:00+09:00
    +     - change observe event timeout 300 to 60
    +     - fixed observe event error message was undef on http level error.
    +
     0.01 2013-03-14T18:50:00+09:00
          - initial version

--- lib/AnyEvent/Lingr.pm
+++ lib/AnyEvent/Lingr.pm
@@ -1,7 +1,7 @@
 package AnyEvent::Lingr;
 use Mouse;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use AnyEvent::HTTP;
 
@@ -256,7 +256,7 @@ sub _polling {
             $self->_polling;
         }
         else {
-            $self->_on_error($res);
+            $self->_on_error($res, $hdr);
         }
     };
     Scalar::Util::weaken($self);
@@ -446,7 +446,7 @@ Error callbacks.
 
 C<$msg> is error message. If this message is form of "\d\d\d: message" like:
 
-    595: Invalid argument
+    596: Operation timed out
 
 This is http level or connection level error. Otherwise C<$msg> is error message returned from lingr api server.
 
